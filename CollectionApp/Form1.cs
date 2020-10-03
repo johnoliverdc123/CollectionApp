e37259a81cd61b4e3fe20e60bba24a6dc970b0c3
OliverDC <johnoliverdelacruz3@gmail.com>
2020-10-03 10:36:13 +0800

    initial commit

---
+++ CollectionApp/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CollectionApp
+{
+    public partial class Form1 : Form
+    {
+        ArrayList fruitlist;
+        public Form1()
+        {
+            
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            fruitlist = new ArrayList();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            String fruitText = textBox1.Text;
+            fruitlist.Add(fruitText);
+            listBox1.Items.Clear();
+
+            foreach(String item in fruitlist)
+            {
+                listBox1.Items.Add(item);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            String fruitText = listBox1.SelectedItem.ToString();
+            fruitlist.Remove(fruitText);
+            listBox1.Items.Clear();
+
+            foreach (String item in fruitlist)
+            {
+                listBox1.Items.Add(item);
+            }
+        }
+    }
+}
---
+++ CollectionApp/Form2.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CollectionApp
+{
+    public partial class Form2 : Form
+    {
+        Hashtable fruitDictionary;
+
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            String fruitSearch = textBox1.Text;
+            String fruitValue = fruitDictionary[fruitSearch].ToString();
+            textBox2.Text = fruitValue;
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            fruitDictionary = new Hashtable();
+
+            fruitDictionary.Add("Apple", 98.50);
+            fruitDictionary.Add("Banana", "28.50");
+            fruitDictionary.Add("Grapes", "Violet");
+            fruitDictionary.Add("Orange", 15);
+
+            listBox1.Items.Clear();
+            foreach (String key in fruitDictionary.Keys)
+            {
+                listBox1.Items.Add(key + " : " + fruitDictionary[key].ToString());
+            }
+        }
+    }
+}
